a747195eca4f8fe56b2c83ed326613477fd90fb1
furkan harmancı <rocklee34@hotmail.com>
2023-09-15 01:45:15 +0300

    refactor: method change has been made 📦

--- app/src/main/java/com/example/navigationkotlin/MainActivity.kt
+++ app/src/main/java/com/example/navigationkotlin/MainActivity.kt
@@ -8,22 +8,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun firstButton(view : View) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-
-        val firstFragment = FirstFragment()
-
-        fragmentTransaction.replace(R.id.mainFrameComponent,firstFragment).commit()
-    }
-    fun secondButton(view : View) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-
-        val secondFragment = SecondFragment()
-
-        fragmentTransaction.replace(R.id.mainFrameComponent,secondFragment).commit()
     }
 }
