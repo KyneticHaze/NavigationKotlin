baeb4eff6d7c56ac2be2d8f07176ca9c9536d76e
furkan harmancı <rocklee34@hotmail.com>
2023-09-15 13:04:54 +0300

    feat: Navigation code added :sparkles:

--- app/src/main/java/com/example/navigationkotlin/FirstFragment.kt
+++ app/src/main/java/com/example/navigationkotlin/FirstFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationkotlin.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -14,8 +18,22 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val firstButton = binding.firstFragmentButton
+
+        firstButton.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment()
+
+            Navigation.findNavController(it).navigate(action)
+
+        }
     }
 }
--- app/src/main/java/com/example/navigationkotlin/SecondFragment.kt
+++ app/src/main/java/com/example/navigationkotlin/SecondFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationkotlin.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,8 +18,21 @@ class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val secondButton = binding.secondFragmentButton
+
+        secondButton.setOnClickListener {
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }
